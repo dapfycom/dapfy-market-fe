4d028331422573e96197a990abf995a183df63b2
Mandy9943 <armandoc9943@gmail.com>
2024-10-27 00:44:03 -0400

    Refactor: Simplify checkout flow
    - Removed unnecessary user data fetching and redirect logic
    
      from the checkout page.
    
    - Directly fetch the client secret using the auth token from
    
      cookies.
    
    - This simplifies the checkout process and improves
    
      performance.

--- src/app/(main)/checkout/[productId]/page.tsx
+++ src/app/(main)/checkout/[productId]/page.tsx
@@ -1,37 +1,23 @@
 import { AUTH_TOKEN_KEY } from "@/config";
-import axiosInstance, { BASE_URL } from "@/utils/axios";
+import { BASE_URL } from "@/utils/axios";
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import CheckoutFormClient from "./client";
 
 const CheckoutForm = async ({ params }: { params: { productId: string } }) => {
   const { productId } = params;
 
-  let userData;
-  try {
-    const { data: user } = await axiosInstance.get("/auth/me", {
-      headers: {
-        Authorization: `Bearer ${cookies().get(AUTH_TOKEN_KEY)?.value}`,
-      },
-    });
-    userData = user;
-  } catch (error) {
-    userData = null;
-  }
-
-  if (!userData) {
-    redirect("/");
-  }
   let clientSecret: string | null;
 
   try {
+    const authToken = cookies().get(AUTH_TOKEN_KEY)?.value;
+
     const clientSecretResponse = await fetch(
       BASE_URL + "/stripe" + "/create-checkout-session",
       {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${cookies().get(AUTH_TOKEN_KEY)?.value}`,
+          Authorization: `Bearer ${authToken}`,
         },
         body: JSON.stringify({
           productID: productId,
